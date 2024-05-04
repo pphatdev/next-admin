e346468a2969168da3d8036ee71b7e05c36ad1e6
pphatdev <leatsophat02@gmail.com>
2024-05-04 11:17:53 +0700

    Create Login Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    return (
-        <></>
-    );
+    return redirect('/login');
 }
