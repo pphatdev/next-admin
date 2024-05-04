e17bc3750474d5360972f6090e9f1a9b2778e6ae
pphatdev <leatsophat02@gmail.com>
2024-05-04 09:36:05 +0700

    Create Error Helper

---
+++ src/helpers/error.ts
@@ -0,0 +1,12 @@
+export class APIError extends Error {
+    status: any;
+    errors: any;
+
+    constructor(message: string, code: any, errors: any) {
+        super(message);
+
+        this.message = message;
+        this.status = code;
+        this.errors = errors;
+    }
+}
