395306a38caa56974739b0df08584ff8ba26e70c
pphatdev <leatsophat02@gmail.com>
2024-05-05 11:22:18 +0700

    components: new collepse button

---
+++ src/components/layouts/collapse-button.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { ButtonType as T } from "@/types/global";
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
+
+const CollapseButton: React.FC<T& { onClick?: ( e: React.MouseEvent )=> void }> = ({
+    className,
+    children,
+    href,
+    props,
+    onClick
+}) => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const route = useRouter();
+    return (
+        <button
+            className={className}
+            ref={buttonRef}
+            onClick={onClick}
+            // onClick={() => href && route.push(href)}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default CollapseButton
